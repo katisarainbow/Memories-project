173f3596b9293a8dca582a056a8922b503334fb2
Kat <katietesara@gmail.com>
2022-02-10 07:45:04 -0400

    Creating form

--- client/src/components/Auth/Auth.js
+++ client/src/components/Auth/Auth.js
@@ -1,11 +1,56 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Avatar, Typography, Button, Paper, Grid, Container, TextField } from '@material-ui/core';
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+
+import useStyles from './styles';
+import Input from './Input';
 
 const Auth = () => {
-    const state = null;
+    const classes = useStyles();
+    const [ showPassword, setShowPassword ] = useState(false);
+
+    const isSignup = true;
+
+    const handleShowPassword = () => {
+        setShowPassword((prevShowPassword) => !prevShowPassword) 
+    }
+
+    const handleSubmit = () => {
+
+    };
+
+    const handleChange = () => {
+
+    };
+
     return(
-        <div>
-            Auth
-        </div>
+        <Container component="main" maxWidth="xs">
+            <Paper className={classes.paper} elevation={3}>
+                <Avatar className={classes.avatar}>
+                    <LockOutlinedIcon/>
+                </Avatar>
+                <Typography variant='h5'>{isSignup ? 'Sign Up' : 'Sign In'}</Typography>
+                <form className={classes.form} onSubmit={handleSubmit}>
+                    <Grid container spacing={2}>
+                        {
+                            isSignup && (
+                                <>
+                                    <Input name="firstName" label="First Name" handleChange={handleChange} autoFocus half />
+                                    <Input name="firstName" label="First Name" handleChange={handleChange}  half />
+                                </>
+                            )
+                        }
+
+                        <Input name="email" label="Email Address" handleChange={handleChange} type="email"/>
+                        <Input name="password" label="Password" handleChange={handleChange} type={ showPassword ? "text" : "password"} handleShowPassword={handleShowPassword}/>
+                        { isSignup && <Input name="confirmPassword" label="Repeat Password" handleChange={handleChange} type={ showPassword ? "text" : "password"} handleShowPassword={handleShowPassword}/>}
+                    </Grid>
+                    <Button type="submit" fullWidth variant="contained" color="primary" className={classes.submit}>
+                        { isSignup ? "Sign Up" : "Sign In"}
+                    </Button>
+                </form>
+            </Paper>
+        </Container>
     )
 }
 
